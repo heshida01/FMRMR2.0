0a24c303187176e9b995712bc12193a764554edd
heshida <heshida@tju.edu.cn>
2020-03-25 17:52:55 +0800

    first commit fe

---
+++ Fmrmd2.0.py
@@ -0,0 +1,172 @@
+import argparse
+import os
+import subprocess
+import sys
+import uuid
+from Bio import  SeqIO
+from collections import OrderedDict
+from fmrmd_parse_args import parse_args
+sys.path.extend('./format')
+sys.path.extend('.')
+
+from format import pos_neg2csv
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    combine_file = os.path.dirname(__file__) + os.sep + 'Temp' + os.sep
+    samples_len = 0
+    inputfile_label_len = OrderedDict()
+    samples = []
+    for inputfile in args.InputFiles:
+        samples += list(SeqIO.parse(inputfile, "fasta"))
+        samples_len += len(list(SeqIO.parse(inputfile, "fasta")))
+        combine_file += f"{inputfile}."
+
+        label = os.path.basename(inputfile).split('.')[0]
+        inputfile_label_len[label] = len(list(SeqIO.parse(inputfile, "fasta")))
+
+    combine_file = combine_file + 'fasta'
+    SeqIO.write(samples, combine_file, "fasta")
+
+
+    """
+    --InputFiles 1.positive.txt 0.negative.txt --FE_method iFeature.py --type AAC
+    """
+    if 'ifeature' in str(args.FE_method_file).lower():
+        combine_file = os.path.dirname(__file__) + os.sep + 'Temp' + os.sep
+        samples_len = 0
+        inputfile_label_len=OrderedDict()
+        samples = []
+        for inputfile in args.InputFiles:
+            samples += list(SeqIO.parse(inputfile, "fasta"))
+            samples_len += len(samples)
+            combine_file += f"{inputfile}."
+
+            label = os.path.basename(inputfile).split('.')[0]
+            
+            inputfile_label_len[label]=len(list(SeqIO.parse(inputfile, "fasta")))
+
+        combine_file = combine_file+'fasta'
+        SeqIO.write(samples, combine_file, "fasta")
+
+        # pog = list(SeqIO.parse(args.posFile, "fasta"))
+        # pog_len = len(pog)
+        # neg = list(SeqIO.parse(args.negFile, "fasta"))
+        # neg_len = len(neg)
+        #
+        # combine_file = os.path.dirname(__file__)+os.sep+'Temp'+os.sep+f"{args.posFile+'.'+args.negFile}"
+        #SeqIO.write(pog+neg, combine_file, "fasta")
+
+        ifeature_parameters = {"type":args.type,
+                               "path":args.path,
+                               "train":args.train,
+                               "label":args.label,
+                               "order":args.order,
+                               "userDefinedOrder":args.userDefinedOrder,
+                               "subtype":args.subtype,
+                               "ktuple":args.ktuple,
+                               "gap_lambda":args.gap_lambda,
+                               "raactype":args.raactype,
+                               "show":args.show}
+        notNull_parameters = ['--' + key + ' ' + ifeature_parameters[key] for key in ifeature_parameters if ifeature_parameters[key] != '' ]
+        str_parameters = ' '.join(notNull_parameters)
+
+        ifeaturecmd = f"python iFeature/{args.FE_method_file} --file {combine_file} --out {combine_file}.out {str_parameters}"
+        print(ifeaturecmd)
+
+        #####https://blog.csdn.net/lilong117194/article/details/74936407
+        sp = subprocess.call(ifeaturecmd.split(), universal_newlines=False,stdout=subprocess.PIPE)
+        #output, err = sp.communicate()
+        # output.decode("utf-8")
+        print(inputfile_label_len)
+
+
+        _,csvfilepath = pos_neg2csv.Process_iFeature_out(combine_file+'.out',inputfile_label_len)
+        print(csvfilepath)
+
+
+        #metricfile = ''.join(map(str,args.posFile.split('.')[:-1]))+'_'+''.join(map(str,args.negFile.split('.')[:-1]))+'.metrics.csv'
+        metricfile = combine_file.strip("fasta")+'metrics.csv'
+        #rdfile = ''.join(map(str,args.posFile.split('.')[:-1]))+'_'+''.join(map(str,args.negFile.split('.')[:-1]))+'.result.csv'
+        rdfile = combine_file.strip("fasta")+'result.csv'
+        mrmd2_parameters = {"l": args.step_length, "m": args.m_topFeatures, "t": args.m}
+        notNull_parameters = ['-' + key + ' ' + mrmd2_parameters[key] for key in mrmd2_parameters if
+                              mrmd2_parameters[key] != '']
+        str_parameters = ' '.join(notNull_parameters)
+        mrmdcmd3 = f"python  mrmd2.0.py  -i {csvfilepath} -o {metricfile}  -c {rdfile} {str_parameters}"
+
+        print(mrmdcmd3)
+        sp3 = subprocess.Popen(mrmdcmd3.split(), universal_newlines=True, stdout=subprocess.PIPE)
+        sp3.communicate()
+
+
+
+
+    else:
+        """
+        Pse in one 2.0
+        --InputFiles 1.positive.txt 0.negative.txt --FE_method pse.py --sequenceType RNA --method kmer --k 5  --step_length 100
+        """
+        opt_Pseinone_parameters=OrderedDict({'k':args.k,'multi':args.multi,'l':args.l,'f':args.f,'n':args.n,'r':args.r,'w':args.w,'lag':args.lag,'oli':args.oli,'m':args.m,
+        'delta':args.delta,'ps':args.ps,'ns':args.ns,'max_dis':args.max_dis,'cp':args.cp,'lamada':args.lamada,'i':args.i,'e':args.e,'all_index':args.all_index,
+        'no_all_index':args.no_all_index})
+
+        if opt_Pseinone_parameters['lamada'] == '-999' and 'pse'in args.FE_method_file.lower():
+            opt_Pseinone_parameters['lamada'] = '2'
+        elif opt_Pseinone_parameters['lamada'] == '-999' and 'acc'in args.FE_method_file.lower():
+            opt_Pseinone_parameters['lamada'] = '1'
+
+
+        notNull_parameters = ['-' + key + ' ' + opt_Pseinone_parameters[key] for key in opt_Pseinone_parameters if opt_Pseinone_parameters[key] != '' ]
+
+        str_parameters = ' '.join(notNull_parameters)
+        print(str_parameters)
+        # if args.k != '':
+        #     opt_Pseinone_parameters['-k'] = args.k
+        #     opt_Pseinone_parameters.move_to_end('-k', last=False)
+
+
+        #cmd = "python --posFile {} --negFile --sequenceType --method --K"
+        ##posout1 = os.path.dirname(__file__)+os.sep+'Temp'+os.sep+args.posFile+str(uuid.uuid1())+'.out'
+        ##negout2 = os.path.dirname(__file__)+os.sep+'Temp'+os.sep+args.negFile+str(uuid.uuid1())+'.out'
+        combine_file_out = os.path.dirname(__file__)+os.sep+'Temp'+os.sep+os.path.basename(combine_file)+str(uuid.uuid1())+'.out'
+        ##poscmd1 = f"python {'PseinOne/'+args.FE_method_file} {args.posFile} {posout1}  {args.sequenceType} {args.method}  {str_parameters}"
+        ##negcmd2 = f"python {'PseinOne/'+args.FE_method_file} {args.negFile} {negout2}  {args.sequenceType} {args.method}  {str_parameters}"
+        combine_file_cmd = f"python {'PseinOne/'+args.FE_method_file} {combine_file}  {combine_file_out} {args.sequenceType} {args.method}  {str_parameters} "
+        print(str_parameters,args.method)
+        # #print(poscmd1)
+        # #sp1 = subprocess.Popen(poscmd1.split(), universal_newlines=True,stdout=subprocess.PIPE)
+        # #sp1.communicate()
+        # #print(negcmd2)
+        # #sp2 = subprocess.Popen(negcmd2.split(), universal_newlines=True,stdout=subprocess.PIPE)
+        # #sp2.communicate()
+        print(combine_file_cmd)
+        sp1 = subprocess.Popen(combine_file_cmd.split(), universal_newlines=True, stdout=subprocess.PIPE)
+
+        #inputcsv,_df = pos_neg2csv.combine_pos_neg(combine_file_out)
+        _, inputcsv = pos_neg2csv.Process_iFeature_out_PSeInOne(combine_file + '.out', inputfile_label_len)
+        print(inputcsv)
+
+
+        #metricfile = ''.join(map(str,args.posFile.split('.')[:-1]))+'_'+''.join(map(str,args.negFile.split('.')[:-1]))+'.metrics.csv'
+        #rdfile = ''.join(map(str,args.posFile.split('.')[:-1]))+'_'+''.join(map(str,args.negFile.split('.')[:-1]))+'.result.csv'
+        metricfile = metricfile = combine_file.strip("fasta")+'metrics.csv'
+        rdfile = combine_file.strip("fasta")+'result.csv'
+        #mrmdcmd3 = f"python  mrmd2.0.py  -i {inputcsv} -o {metricfile}  -c {rdfile} "
+        #print(mrmdcmd3)
+        mrmd2_parameters = {"l": args.step_length, "m": args.m_topFeatures, "t": args.m}
+        notNull_parameters = ['-' + key + ' ' + mrmd2_parameters[key] for key in mrmd2_parameters if
+                              mrmd2_parameters[key] != '']
+        str_parameters = ' '.join(notNull_parameters)
+
+        mrmdcmd3 = f"python  mrmd2.0.py  -i {inputcsv} -o {metricfile}  -c {rdfile} {str_parameters}"
+        print(mrmdcmd3)
+        sp3 = subprocess.Popen(mrmdcmd3.split(), universal_newlines=True, stdout=subprocess.PIPE)
+        sp3.communicate()
+
+    print('exit')
+
+
+
---
+++ fmrmd_parse_args.py
@@ -0,0 +1,180 @@
+import  argparse
+def parse_args():
+    parser = argparse.ArgumentParser()
+    #     description=
+    #     "Visualzes the features based on t-sne in a 2D feature space")
+
+    # parser.add_argument(
+    #
+    #     '--posFile',type=str,
+    #     required=True)
+    #
+    # parser.add_argument(
+    #
+    #     '--negFile',type=str,
+    #     required=True)
+    parser.add_argument(
+        #type=argparse.FileType('r'),
+        '--InputFiles',nargs=argparse.ONE_OR_MORE,type=str,
+        required=True)
+
+
+    parser.add_argument(
+
+        '--FE_method_file',type=str,
+        )
+
+
+
+#####################Pse in One 2.0 可选参数####################
+##################################################################
+    parser.add_argument(
+
+        '--sequenceType',choices=['DNA', 'RNA', 'Protein'],type=str,
+        )
+
+
+    parser.add_argument(
+
+        '--method',type=str,help="pse in one ,bulit in method"
+        )
+
+
+    parser.add_argument('--k', type=str,default='',
+                       help="sc：The number of k adjacent structure statuses. default=2. It works only with PseSSC method.\
+    				pse:The value of kmer, it works only with PseKNC method.\
+    				nac:For Kmer, IDKmer, mismatch, subsequence methods. The k value of kmer.")
+
+    parser.add_argument('--multi', type=str, default='', choices=['0', '1'],
+                        help="Whether binary classification or multiclass classification.\n"
+                             "0: binary classification, default value.\n"
+                             "1: multiclass classification.")
+
+    parser.add_argument('--l',type=str,default='',
+                        help="The libSVM output file label.\n"
+                             "For binary classification problem, the labels can only be '+1' or '-1'.\n"
+                             "For multiclass classification problem, the labels can be set as an integer.")
+
+    parser.add_argument('--f', default='', choices=['tab', 'svm', 'csv'],
+                        help="The output format (default = tab).\n"
+                             "tab -- Simple format, delimited by TAB.\n"
+                             "svm -- The libSVM training data format.\n"
+                             "csv -- The format that can be loaded into a spreadsheet program.")
+
+    parser.add_argument('--n', type=str,default='',
+                        help="sc: The maximum distance between structure statuses. default=0. It works only with PseDPC method.")
+
+    parser.add_argument('--r', type=str,default='',
+                        help="sc: The value of lambda, represents the highest counted rank (or tier) of the structural correlation along a RNA chain. default=2.\
+    				      nac: Whether consider the reverse complement or not. 1 means True, 0 means False. (default = 0)")
+
+    parser.add_argument('--w', type=str,default='',
+                        help="sc: The weight factor used to adjust the effect of the correlation factors. default=0.1.\
+    					 pse: The value of weight. default=0.1")
+
+
+
+    parser.add_argument('--lag', type=str,default='',
+                        help="acc: The value of lag.")
+
+    parser.add_argument('-oli', type=str,default='', choices=['0', '1'],
+                        help="acc: Choose one kind of Oligonucleotide: 0 represents dinucleotide;\n"
+                             "1 represents trinucleotide.")
+
+
+
+
+
+    parser.add_argument('--m', type=str,default='',help="nac: For mismatch. The max value inexact matching. (m<k)")
+    parser.add_argument('--delta', type=str,default='',
+                        help="nac: For subsequence method. The value of penalized factor. (0<=delta<=1)")
+
+    parser.add_argument('--ps',type=str,default='',
+                        help="nac: The input positive source file in FASTA format for IDKmer.")
+    parser.add_argument('-ns',type=str,default='',
+                        help="nac: The input negative source file in FASTA format for IDKmer.")
+    parser.add_argument('--max_dis', type=str,default='',
+                        help="nac: For DR and Distance Pair methods. The max distance value of DR and Distance Pair.")
+    parser.add_argument('--cp', type=str,default='', choices=['cp_13', 'cp_14', 'cp_19', 'cp_20'],
+                        help="nac: For Distance Pair method. The reduced alphabet scheme. Choose one of the four:\n"
+                             "cp_13, cp_14, cp_19, cp_20 ")
+
+
+    parser.add_argument('--lamada', type=str,default='',
+                        help="pse: The value of lamada. default=2\
+    					   acc: The value of lamada. default=1")
+
+    parser.add_argument('--i',type=str,default='',
+                        help="pse and acc: The indices file user choose.\n"
+                             "Default indices:\n"
+                             "DNA dinucleotide: Rise, Roll, Shift, Slide, Tilt, Twist.\n"
+                             "DNA trinucleotide: Dnase I, Bendability (DNAse).\n"
+                             "RNA: Rise, Roll, Shift, Slide, Tilt, Twist.\n"
+                             "Protein: Hydrophobicity, Hydrophilicity, Mass.")
+
+    parser.add_argument('--e', type=str,default='',help="pse and acc: The user-defined indices file.\n")
+
+    parser.add_argument('--all_index',default='', action='store_true',
+                        help="pse and acc : Choose all physicochemical indices")
+
+    parser.add_argument('--no_all_index', default='', action='store_false',
+                        help="pse and acc : Do not choose all physicochemical indices, default.")
+
+    #####################
+
+
+
+
+
+
+    ####iFeature
+    #parser.add_argument("--file", required=True, help="input fasta file")
+    parser.add_argument("--type", default='',
+                        choices=['AAC', 'EAAC', 'CKSAAP', 'DPC', 'DDE', 'TPC', 'BINARY',
+                                 'GAAC', 'EGAAC', 'CKSAAGP', 'GDPC', 'GTPC',
+                                 'AAINDEX', 'ZSCALE', 'BLOSUM62',
+                                 'NMBroto', 'Moran', 'Geary',
+                                 'CTDC', 'CTDT', 'CTDD',
+                                 'CTriad', 'KSCTriad',
+                                 'SOCNumber', 'QSOrder',
+                                 'PAAC', 'APAAC',
+                                 'KNNprotein', 'KNNpeptide',
+                                 'PSSM', 'SSEC', 'SSEB', 'Disorder', 'DisorderC', 'DisorderB', 'ASA', 'TA'
+                                 ],
+
+                        help="the encoding type")
+    parser.add_argument("--path", default='',
+                        help="data file path used for 'PSSM', 'SSEB(C)', 'Disorder(BC)', 'ASA' and 'TA' encodings")
+    parser.add_argument("--train", default='',
+                        help="training file in fasta format only used for 'KNNprotein' or 'KNNpeptide' encodings")
+    parser.add_argument("--label",  default='',
+                        help="sample label file only used for 'KNNprotein' or 'KNNpeptide' encodings")
+    parser.add_argument("--order", dest='order', default='',
+                        choices=['alphabetically', 'polarity', 'sideChainVolume', 'userDefined'],
+                        help="output order for of Amino Acid Composition (i.e. AAC, EAAC, CKSAAP, DPC, DDE, TPC) descriptors")
+    parser.add_argument("--userDefinedOrder", dest='userDefinedOrder', default='',
+                        help="user defined output order for of Amino Acid Composition (i.e. AAC, EAAC, CKSAAP, DPC, DDE, TPC) descriptors")
+    parser.add_argument("--out", dest='outFile', default='',
+                        help="the generated descriptor file")
+
+    ####iFeaturePseKRAAC.py
+    parser.add_argument("--subtype", choices=['g-gap', 'lambda-correlation'],default='',
+                        help="the subtype of the descriptor type, default is 'g-gap'")
+    parser.add_argument("--ktuple", choices=[1, 2, 3], default='',help="k-tuple peptide, default is 2", type=str)
+    parser.add_argument("--gap_lambda", choices=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],default='',
+                        help="the gap value or lambda value for the 'g-gap' model or 'lambda-correlation' model",
+                        type=str)
+    parser.add_argument("--raactype", default='',help="the reduced amino acids cluster type", type=str)
+    parser.add_argument("--show", default='',help="show detatiled available '--raactype' value for each type",
+                        action="store_true")
+
+    # ktuple  gap_lambda    raactype  int:type
+    ####mrmd2.0###############
+
+    parser.add_argument("--step_length",  type=str, default='', help="step length default=1" )
+    parser.add_argument("--m_topFeatures", type=str, default='', help="mrmd2.0 features top n default=auto")
+    parser.add_argument("--t", type=str, default='', help="metric basline default=f1" )
+
+    args = parser.parse_args()
+
+    return args
